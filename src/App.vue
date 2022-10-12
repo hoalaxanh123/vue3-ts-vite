1964bbd704487834002d50b0eb91f564ef26eb7c
Vuong Nguyen <vuong.n@xefino.com>
2022-10-12 19:18:06 +0700

    Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,23 @@
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" title="Vite">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank" title="Vue">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer">
+      <!--  -->
+    </v-navigation-drawer>
 
-    <a
-      href="https://vuejs.org/guide/typescript/overview.html"
-      target="_blank"
-      title="TypeScript"
-    >
-      <img src="./assets/TS.png" class="logo ts" alt="TypeScript logo" />
-    </a>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-    <a href="https://eslint.vuejs.org/" target="_blank" title="Eslint">
-      <img src="./assets/Eslint.png" class="logo eslint" alt="Eslint logo" />
-    </a>
+      <v-toolbar-title>Welcome to Vuetify</v-toolbar-title>
+    </v-app-bar>
 
-    <a
-      href="https://www.npmjs.com/package/eslint-plugin-prettier-vue"
-      target="_blank"
-      title="Prettier"
-    >
-      <img
-        src="./assets/Prettier.png"
-        class="logo prettier"
-        alt="Prettier logo"
-      />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue + TS + Eslint + Prettier" />
+    <v-main>
+      <!--  -->
+    </v-main>
+  </v-app>
 </template>
 
-<script setup lang="ts">
-  // This starter template is using Vue 3 <script setup> SFCs
-  // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import HelloWorld from './components/HelloWorld.vue'
+<script>
+  export default {
+    data: () => ({ drawer: null }),
+  }
 </script>
-
-<style scoped>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
-  }
-  .logo.ts:hover {
-    filter: drop-shadow(0 0 2em #0482d6aa);
-  }
-  .logo.eslint:hover {
-    filter: drop-shadow(0 0 2em #034875aa);
-  }
-  .logo.prettier:hover {
-    filter: drop-shadow(0 0 2em #d14ee2d8);
-  }
-</style>
